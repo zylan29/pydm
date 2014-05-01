513da5441abc8c734d8a1f5af2851515ea71a4fe
Ziyang Li <lzynudt@gmail.com>
2014-05-01 22:38:36 +0800

    change version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ AUTHOR_EMAIL="lzynudt@gmail.com"
 URL="https://github.com/anzigly/pydm"
 setup(
     name=NAME,
-    version="0.2.2",
+    version="0.2.4",
     description=DESCRIPTION,
     long_description=open("README.md").read(),
     author=AUTHOR,
