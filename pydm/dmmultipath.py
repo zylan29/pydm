2542f12a5026ab6cf20743a581cb548201f7207c
anzigly <anzigly@gmail.com>
2014-08-12 17:20:56 +0800

    add DmMultipath

--- pydm/dmmultipath.py
+++ pydm/dmmultipath.py
@@ -2,4 +2,22 @@ from pydm.dmtable import Table
 
 
 class DmMultipath(Table):
-    pass
+
+    def __init__(self, name, root_helper=''):
+        super(DmMultipath, self).__init__(name, 'multipath', root_helper=root_helper)
+        if self.existed:
+            self._parse_table()
+        else:
+            self.size = 0
+            self.disks = []
+
+    def __str__(self):
+        count = len(self.disks)
+        multipath_table = '0 %d multipath 0 0 1 1 queue-length 0 %d 1 ' % (self.size, count)
+        for disk in self.disks:
+            multipath_table += disk + ' 128 '
+        return multipath_table
+
+    def _parse_table(self):
+        #TODO: to be continued...
+        return NotImplementedError()
